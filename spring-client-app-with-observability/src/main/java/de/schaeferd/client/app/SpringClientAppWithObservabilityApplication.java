19f66dcefb246323cbf3f692af3aca734be75613
Dennis Schäfer <dennis.schaefer94@gmail.com>
2025-10-19 19:42:43 +0200

    Observability setup

---
+++ spring-client-app-with-observability/src/main/java/de/schaeferd/client/app/SpringClientAppWithObservabilityApplication.java
@@ -0,0 +1,13 @@
+package de.schaeferd.client.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClientAppWithObservabilityApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringClientAppWithObservabilityApplication.class, args);
+    }
+}
---
+++ spring-client-app-with-observability/src/test/java/de/schaeferd/client/app/SpringClientAppWithObservabilityApplicationTests.java
@@ -0,0 +1,15 @@
+package de.schaeferd.client.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClientAppWithObservabilityApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
